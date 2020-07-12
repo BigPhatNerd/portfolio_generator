63e8beaedb00731d5fcd657aa1ba2d0424a04ff6
Wilson <wilsonhorrell@git.training>
2020-07-12 15:12:26 -0500

    Broke as a joke.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
 const fs = require("fs");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const promptUser = () => {
     return inquirer
         .prompt([{
@@ -54,7 +55,7 @@ const promptProject = portfolioData => {
 =========================
 Add a New promptProject
 =========================
-		`);
+        `);
     return inquirer.prompt([{
                 type: 'input',
                 name: 'name',
@@ -126,13 +127,20 @@ Add a New promptProject
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile("./index.html", pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log("Page created! Check out index.html in this directory to see it!");
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
 
 
--- src/page-template.js
+++ src/page-template.js
@@ -7,54 +7,53 @@ const generateAbout = aboutText => {
   <h2 class="text-dark bg-primary p-2 display-inline-block">
   <p>${aboutText}</p>
   </section>`
-}
+};
 
 const generateProjects = projectsArr => {
     return `
-    <section class="my-3" id="portfolio">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-      <div class="flex-row justify-space-between">
-      ${projectsArr
-        .filter(({ feature }) => feature)
-        .map(({ name, description, languages, link }) => {
-          return ` <
+  <section class="my-3" id="portfolio">
+  <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+  <div class="flex-row justify-space-between">
+  ${projectsArr
+    .filter(({ feature }) => feature)
+    .map(({ name, description, languages, link }) => {
+      return ` <
         div class = "col-12 mb-2 bg-dark text-light p-3" >
         <h3 class="portfolio-item-title text-light">${name}</h3> <
         h5 class = "portfolio-languages" >
         Built With:
         $ { languages.join(', ') } <
-        /h5> <
-    p > $ { description } < /p> <
-    a href = "${link}"
+        h / 5 >
+        <p>${description}</p> <
+        a href = "${link}"
     class = "btn" > <i class="fab fa-github mr-2"></i>
-    View Project on GitHub < /a> < /
-        div >
-        `;
-        })
-        .join('')}
-
-      ${projectsArr
-        .filter(({ feature }) => !feature)
-        .map(({ name, description, languages, link }) => {
-          return ` <
-        div class = "col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column" >
-        <h3 class="portfolio-item-title text-light">${name}</h3> <
+    View Project on Github < /a> <
+        /div>
+    `;
+    })
+    .join('')}
+    ${projectsArr
+      .filter(({ feature }) => !feature)
+      .map(({ name, description, languages, link }) => {
+        return ` <
+    div class = "col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column" >
+        <h3 class="portfolio-itme-title text-light">${name}</h3> <
         h5 class = "portfolio-languages" >
         Built With:
         $ { languages.join(', ') } <
         /h5> <
-    p > $ { description } < /p> <
-    a href = "${link}"
-    class = "btn mt-auto" > <i class="fab fa-github mr-2"></i>
-    View Project on GitHub < /a> < /
-        div >
-        `;
-        })
-        .join('')}
+        p > $ { description } < /p> <
+        a href = "${link}"
+    class = "btn mt-auto" > < i class = "fab fa-githubmr-2></i>View Project on Github</a> <
+        /div>
+    `;
+      })
+      .join('')}
       </div>
-    </section>
-  `;
+      </section>
+      `;
 };
+
 module.exports = templateData => {
 
     const { projects, about, ...header } = templateData;
